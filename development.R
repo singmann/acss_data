50f40a6f0a571a363962498caf32a120c107e18f
Henrik Singmann <singmann@gmail.com>
2025-05-16 09:11:07 +0100

    removed namespace

--- development.R
+++ development.R
@@ -1,6 +1,7 @@
 
 devtools::build(path = "development")
 devtools::check_built("development/acss.data_1.1.tar.gz")
+devtools::install_local("development/acss.data_1.1.tar.gz")
 
 rhub::rhub_setup()
 rhub::rhub_doctor() 
